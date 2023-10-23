5b34712c01d56cc6d471fb8b24b0594c2698a251
Manas Tripathi <TRIPATHIMANAS98@GMAIL.COM>
2023-10-23 20:12:22 +0530

    animation + tabs on orders card

--- src/layouts/OrdersLayout.js
+++ src/layouts/OrdersLayout.js
@@ -1,11 +1,22 @@
 import PlaceOrderCard from "@/components/orderComponents/PlaceOrderCard";
 import YieldCurveCard from "@/components/orderComponents/YieldCurveCard";
-import React from "react";
+import React, { useState } from "react";
 
 function OrdersLayout() {
+  const [move, setMove] = useState(false);
+  const [fadeIn, setFadeIn] = useState(false);
+  const handleClick = () => {
+    setMove(!move);
+    setFadeIn(!fadeIn);
+  };
+
   return (
     <div className="flex flex-col items-center space-y-3 w-full mt-8">
-      <div className="border border-[#395251] flex items-center space-x-12 pl-16  w-full bg-[#15191D] py-4">
+      <div
+        className={`border border-[#395251] flex items-center space-x-12 pl-16  w-full bg-[#15191D] py-4 opacity-0 ${
+          fadeIn ? "opacity-100" : ""
+        } transition-opacity duration-500 ease-in-out z-10`}
+      >
         <div className="flex flex-col items-start">
           <span className="text-xl text-white uppercase font-light">
             Transactions
@@ -27,10 +38,18 @@ function OrdersLayout() {
         className="grid space-x-3 w-full"
         style={{ gridTemplateColumns: "600px 1fr" }}
       >
-        <div className="border border-[#395251]">
-          <PlaceOrderCard />
+        <div
+          className={`border border-[#395251] transform ${
+            move ? "translate-x-0" : "translate-x-[65%]"
+          } transition-transform duration-500 ease-in-out z-20`}
+        >
+          <PlaceOrderCard handleClick={handleClick} />
         </div>
-        <div className=" border border-[#395251]">
+        <div
+          className={`border border-[#395251] opacity-0 ${
+            fadeIn ? "opacity-100" : ""
+          } transition-opacity duration-500 ease-in-out z-10`}
+        >
           <YieldCurveCard />
         </div>
       </div>
