fb1d1650607e48fd301888b4a0f2f521742c815c
Manas Tripathi <TRIPATHIMANAS98@GMAIL.COM>
2023-11-16 18:33:01 +0530

    added senTx functions for keplt + metamask

--- src/layouts/DashboardLayout.js
+++ src/layouts/DashboardLayout.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
+import Web3 from "web3";
 
 const navigation = [
   {
@@ -29,6 +30,8 @@ const navigation = [
   },
 ];
 function DashboardLayout({ children, activePage }) {
+  const [publicAddress, setPublicAddress] = useState("");
+  // ! ************************************************************************************************************** KEPLR
   async function getKeplr() {
     if (!window.keplr) {
       alert("Please install Keplr extension");
@@ -91,6 +94,72 @@ function DashboardLayout({ children, activePage }) {
       console.error("Error in sendTransaction:", error);
     }
   }
+  // ! ************************************************************************************************************** METAMASK
+  async function connectMetaMask() {
+    if (typeof window.ethereum !== "undefined") {
+      // MetaMask is installed
+      try {
+        // Request account access
+        const accounts = await window.ethereum.request({
+          method: "eth_requestAccounts",
+        });
+        const chainId = await window.ethereum.request({
+          method: "eth_chainId",
+        });
+
+        // Convert chainId to a decimal number
+        const numericChainId = parseInt(chainId, 16);
+        const account = accounts[0];
+        console.log("Connected account:", account, numericChainId);
+        setPublicAddress(account);
+        // console.log(getTransactionHistory(account));
+        return account; // You can use this account information in your application
+      } catch (error) {
+        console.error("User denied account access");
+      }
+    } else {
+      // MetaMask is not installed
+      alert(
+        "MetaMask is not installed. Please install it to use this feature."
+      );
+    }
+  }
+  async function getTransactionHistory(address) {
+    // Initialize Web3
+    const web3 = new Web3(
+      Web3.givenProvider || "https://sentry.tm.injective.network:443"
+    );
+    // Fetch transaction history for the address
+    // This is a placeholder - you need to use a method compatible with INJ chain
+    const transactions = await web3.eth.getPastLogs({ address: address });
+
+    return transactions;
+  }
+  async function sendTransactionWithMetamask(fromAddress, toAddress, amount) {
+    if (typeof window.ethereum === "undefined") {
+      alert("Please install MetaMask!");
+      return;
+    }
+
+    try {
+      const web3 = new Web3(window.ethereum);
+      await window.ethereum.enable();
+
+      // Specify the transaction parameters
+      const tx = {
+        from: fromAddress,
+        to: toAddress,
+        value: web3.utils.toWei(amount, "ether"), // Convert the amount to Wei
+        gas: 21000, // This is the gas limit for standard transactions
+      };
+
+      // Send the transaction
+      const txHash = await web3.eth.sendTransaction(tx);
+      console.log("Transaction Hash:", txHash);
+    } catch (error) {
+      console.error("Transaction error:", error);
+    }
+  }
 
   return (
     <main
@@ -126,12 +195,22 @@ function DashboardLayout({ children, activePage }) {
             </Link>
           ))}
         </div>
-        <button
-          onClick={connectWallet}
-          className="font-proxima-nova mr-16 flex justify-center rounded-md border-2 border-[#008884] bg-[#008884] py-3 px-6 font-normal text-black hover:border-[#008884] hover:bg-black hover:text-[#008884]"
-        >
-          Connect Wallet
-        </button>
+        {publicAddress ? (
+          <button className="font-proxima-nova mr-16 flex justify-center rounded-md border-2 border-[#008884] bg-[#008884] py-3 px-6 font-normal text-black hover:border-[#008884] hover:bg-black hover:text-[#008884]">
+            {publicAddress.slice(0, 5)}...
+            {publicAddress.slice(
+              publicAddress.length - 4,
+              publicAddress.length
+            )}
+          </button>
+        ) : (
+          <button
+            onClick={connectMetaMask}
+            className="font-proxima-nova mr-16 flex justify-center rounded-md border-2 border-[#008884] bg-[#008884] py-3 px-6 font-normal text-black hover:border-[#008884] hover:bg-black hover:text-[#008884]"
+          >
+            Connect Wallet
+          </button>
+        )}
       </header>
       <section className="max-w-[95vw] mx-auto">{children}</section>
       <footer></footer>
