4485e1488cff01ac3a9577620e436e147e032d84
Jayraj Roshan <jayrajroshan1@gmail.com>
2024-01-10 15:32:45 +0530

    minor fix

--- src/components/orderComponents/PlaceOrderCard.tsx
+++ src/components/orderComponents/PlaceOrderCard.tsx
@@ -39,7 +39,6 @@ import { PlaceOrderCardProps } from '@/types/Components';
 
 import { Window as KeplrWindow } from "@keplr-wallet/types";
 declare global {
-  // eslint-disable-next-line @typescript-eslint/no-empty-interface
   interface Window extends KeplrWindow { }
 }
 const tabs = [
